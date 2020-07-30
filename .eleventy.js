38047722efdb35ecc9dd69b7ee2f6f9faee32204
Giulia Chiola <giulia.chiola.17@gmail.com>
2020-07-30 16:47:33 +0200

    feat: update cards

--- .eleventy.js
+++ .eleventy.js
@@ -1,34 +1,34 @@
 const CleanCSS = require("clean-css");
 
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('images')
-  eleventyConfig.addPassthroughCopy('admin')
-  eleventyConfig.addPassthroughCopy('_includes/styles/css')
+	eleventyConfig.addPassthroughCopy('images')
+	eleventyConfig.addPassthroughCopy('admin')
+	eleventyConfig.addPassthroughCopy('_includes/styles/css')
 
-  /**
-    Date
-   */
-  const {
-    DateTime
-  } = require("luxon");
+	/**
+		Date
+	 */
+	const {
+		DateTime
+	} = require("luxon");
 
-  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-      return DateTime.fromJSDate(dateObj, {
-        zone: 'utc'
-      }).toFormat('yy-MM-dd');
-    });
+	// https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+		eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+			return DateTime.fromJSDate(dateObj, {
+				zone: 'utc'
+			}).toFormat('yy-MM-dd');
+		});
 
-    eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj, {
-      zone: 'utc'
-    }).toFormat('dd LLL yyyy');
-  });
+		eleventyConfig.addFilter("readableDate", dateObj => {
+		return DateTime.fromJSDate(dateObj, {
+			zone: 'utc'
+		}).toFormat('dd LLL yyyy');
+	});
 
-  /**
-    CSS
-  */
-  eleventyConfig.addFilter("cssmin", function(code) {
-    return new CleanCSS({}).minify(code).styles;
-  });
+	/**
+		CSS
+	*/
+	eleventyConfig.addFilter("cssmin", function(code) {
+		return new CleanCSS({}).minify(code).styles;
+	});
 };
